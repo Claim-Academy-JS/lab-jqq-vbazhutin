4e83291732986d8262501c42b8dbdd76b4923a35
Vlad <vladimir.bazhutin@gmail.com>
2020-07-13 17:08:42 -0500

    Outut color in output bar when slider moves, add reset function for slider

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -1,5 +1,5 @@
 export default () => `
   <div class="color input flex flex--column">
     <label for="colorPicker">Color (RGB)</label>
-    <input type="color" name="color" id="colorPicker" />
+    <input type="color" name="color" id="colorPicker" value="#000000"/>
   </div>`
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function render () {
   ${InputColor()}
   ${OutputColor()}
   `
-
+  // defining variables
   const colorPicker = document.querySelector('#colorPicker')
   const HTMLRoot = document.querySelector(':root')
   const toggle = document.querySelector('#switch')
@@ -19,20 +19,41 @@ function render () {
   const sliderText = document.querySelector('#sliderText')
 
   colorPicker.addEventListener('input', function () {
+    reset ()
     HTMLRoot.style.setProperty('--input-color', this.value)
     HTMLRoot.style.setProperty('--output-color', this.value)
   })
 
+  //Resets slider value and text to '0'
+  function reset () {}
+  slider.value = 0
+  sliderText.innerText = 0 + '%'
+}
+
   toggle.addEventListener('change', function () {
     labels.forEach(label => {
-    // DO NOT inclide the '.' when using classlist
+      // DO NOT inclide the '.' when using classlist
       label.classList.toggle('isActive')
     })
   })
 
-  // Slider 0 - 100%
+  // Add listener to lsiten to Lighten/Darken switch button
+
+  // Slider 0 - 100%, Plus output color into output color bar
   slider.addEventListener('input', function () {
     sliderText.innerText = slider.value + '%'
+    console.log(colorPicker.value)
+
+    // converting HEX to HSL and takeing HUE, SATURATION values
+    const hsl = convert.hex.hsl(colorPicker.value)
+    const h = hsl[0]
+    const s = hsl[1]
+    const l = slider.value
+    console.log(h, s, l)
+    console.log(convert.hsl.hex(h, s, l))
+
+    // Converting received data back to hex to assign HEX value to the output bar
+    HTMLRoot.style.setProperty('--output-color', '#' + convert.hsl.hex(h, s, l))
   })
 }
 
