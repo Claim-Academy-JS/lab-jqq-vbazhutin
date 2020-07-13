213beceb6c5463e1202636dba891119e1a067b89
Vlad <vladimir.bazhutin@gmail.com>
2020-07-13 15:44:39 -0500

    Render all components and bring back logic from scripts

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-import convert from 'color-convert'
 import { Picker, InputColor, OutputColor, Slider, Switch } from './components'
+
+import convert from 'color-convert'
+
+function render () {
+  document.querySelector('#root').innerHTML = `
+  ${Picker()}
+  ${Switch()}
+  ${Slider()}
+  ${InputColor()}
+  ${OutputColor()}
+  `
+
+  const colorPicker = document.querySelector('#colorPicker')
+  const HTMLRoot = document.querySelector(':root')
+  const toggle = document.querySelector('#switch')
+  const labels = document.querySelector('.switch-container').querySelectorAll('label')
+  const slider = document.querySelector('#colorSlider')
+  const sliderText = document.querySelector('#sliderText')
+
+  colorPicker.addEventListener('input', function () {
+    HTMLRoot.style.setProperty('--input-color', this.value)
+    HTMLRoot.style.setProperty('--input-color', this.value)
+      .console.log(this.value)
+  })
+
+  toggle.addEventListener('change', function () {
+    labels.forEach(label => {
+    // DO NOT inclide the '.' when using classlist
+      label.classList.toggle('isActive')
+    })
+  })
+
+  // Slider 0 - 100%
+  slider.addEventListener('input', function () {
+    sliderText.innerText = slider.value + '%'
+  })
+}
+
+render()
