3521a96b9fa0d9e63ee1b1bfb51d45c18760b3ed
Vlad <vladimir.bazhutin@gmail.com>
2020-07-13 10:36:23 -0500

    adding node module to convert hex to hsl

--- scripts.js
+++ scripts.js
@@ -5,6 +5,8 @@ const toggleSwitch = document.querySelector('#switch')
 const labels = document.querySelector('.switch-container').querySelectorAll('label')
 const slider = document.querySelector('#color-slider')
 const sliderText = document.querySelector('#slider-text')
+// importing additional node module to convert HEX TO HSL
+const hexToHsl = require('hex-to-hsl')
 
 // Changing input and output color containers according to the input color value
 inputColor.addEventListener('input', function () {
@@ -23,4 +25,5 @@ toggleSwitch.addEventListener('change', function () {
 // Slider 0 - 100%
 slider.addEventListener('input', function () {
   sliderText.innerText = slider.value + '%'
+  HTMLRoot.style.setProperty('--output-color', hexToHsl(inputColor.value))
 })
